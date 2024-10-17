ee826ae89c5ee1b8035c6d4828e1bef7154cb27b
David Kantun <dkantun@gmail.com>
2024-10-16 20:42:31 -0600

    Time Inactives

---
+++ src/app/services/timeinactives.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+import { TrackingService } from './tracking.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TimeinactivesService {
+
+  private http = inject(HttpClient);
+  private trackingService = inject(TrackingService);
+
+  getInactiveTimes(id: number): Observable<any[]> {
+    return this.http.get<any[]>(`${environment.urlSmp}/Timeinactives/byProject/${id}`, { headers: this.trackingService.getHeaders() });
+  }
+
+  addInactiveTime(data: any): Observable<any> {
+    return this.http.post(`${environment.urlSmp}/Timeinactives`, data, { headers: this.trackingService.getHeaders() });
+  }
+
+  updateInactiveTime(id: number, data: any): Observable<any> {
+    return this.http.put(`${environment.urlSmp}/Timeinactives/${id}`, data, { headers: this.trackingService.getHeaders() });
+  }
+
+  deleteInactiveTime(id: number): Observable<any> {
+    return this.http.delete(`${environment.urlSmp}/Timeinactives/${id}`, { headers: this.trackingService.getHeaders() });
+  }
+}
