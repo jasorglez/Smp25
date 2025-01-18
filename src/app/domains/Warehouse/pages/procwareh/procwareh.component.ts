8c0d08abe0b778cafe16077e1af30eceb9abf777
David Kantun <dkantun@gmail.com>
2025-01-17 19:40:14 -0600

    Placeholder page for Employees

--- src/app/domains/Warehouse/pages/procwareh/procwareh.component.ts
+++ src/app/domains/Warehouse/pages/procwareh/procwareh.component.ts
@@ -7,11 +7,12 @@ import { RequisitionsComponent } from "../../components/requisitions/requisition
 import { PurchaseOrderComponent } from "../../components/purchaseorder/purchaseorder.component";
 import { InAndOutComponent } from '../../components/inandout/inandout.component';
 import { SharedModule } from 'app/shared/shared.module';
+import { EmployeesComponent } from '../../components/employees/employees.component';
 
 @Component({
   selector: 'app-procwareh',
   standalone: true,
-  imports: [TranslateModule, SharedModule, WarehousesComponent, MaterialsComponent, RequisitionsComponent, PurchaseOrderComponent, InAndOutComponent],
+  imports: [TranslateModule, SharedModule, WarehousesComponent, MaterialsComponent, RequisitionsComponent, PurchaseOrderComponent, InAndOutComponent, EmployeesComponent],
   templateUrl: './procwareh.component.html',
   styleUrl: './procwareh.component.scss'
 })
