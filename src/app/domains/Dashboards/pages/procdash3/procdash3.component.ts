4bdaffe871be21b9a71d3a3d2e2b522e724e836a
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-03-05 13:19:19 -0600

    Cambios en Rama Develop

---
+++ src/app/domains/Dashboards/pages/procdash3/procdash3.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-procdash3',
+  standalone: true,
+  imports: [],
+  templateUrl: './procdash3.component.html',
+  styleUrl: './procdash3.component.scss'
+})
+export class Procdash3Component {
+
+}
