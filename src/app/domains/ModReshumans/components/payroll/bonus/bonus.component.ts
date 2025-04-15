645c60acdae41b3f9ba965b5f8391409e39fd687
Julio Galicia <jgalicia2008@hotmail.com>
2025-04-14 21:11:03 -0600

    arreglos minimos

--- src/app/domains/ModReshumans/components/payroll/bonus/bonus.component.ts
+++ src/app/domains/ModReshumans/components/payroll/bonus/bonus.component.ts
@@ -110,7 +110,7 @@ export class BonusComponent{
     );
   }
 
-  obtenerDatosEmpleados(){
+  obtenerBonosEmpleados(){
     this.idBranch = this.signalsService.getBranchSelectedBySidebar()();
     console.log("Consulta: ", this.fechaInicio, this.fechaFin, this.idBranch)
     this.administrationService.getEmployeesBonus(this.fechaInicio, this.fechaFin, this.idBranch).subscribe({
@@ -275,7 +275,7 @@ export class BonusComponent{
           this.newlyAddedRows = [];
 
           // Esperar a que los datos se carguen completamente
-          await this.obtenerDatosEmpleados();
+          await this.obtenerBonosEmpleados();
 
           // Esperar un ciclo de renderizado adicional
           await new Promise(resolve => setTimeout(resolve, 0));
@@ -374,7 +374,7 @@ export class BonusComponent{
   }
 
   revert(){
-    this.obtenerDatosEmpleados();
+    this.obtenerBonosEmpleados();
     this.notSavedChanges = false;
   }
 
@@ -409,7 +409,7 @@ export class BonusComponent{
       const datos = this.selectFechas.value;
       this.fechaInicio = datos.fechaInicio
       this.fechaFin = datos.fechaFin
-      this.obtenerDatosEmpleados()
+      this.obtenerBonosEmpleados()
     }
   }
 
