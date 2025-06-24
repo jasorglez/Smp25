9dd520c40e0f3a52ca9d0ba3638fdc44dde40267
David Kantun <dkantun@gmail.com>
2025-06-24 13:50:12 -0600

    Arreglo en la configuracion del checador

--- src/app/domains/ModReshumans/components/setup/clock/clock.component.ts
+++ src/app/domains/ModReshumans/components/setup/clock/clock.component.ts
@@ -8,11 +8,11 @@ import { ReactiveFormsModule, FormBuilder, FormGroup, Validators, FormControl } 
 @Component({
   selector: 'app-clock',
   standalone: true,
-  imports: [ ReactiveFormsModule ],
+  imports: [ReactiveFormsModule],
   templateUrl: './clock.component.html',
 })
 export class ClockComponent {
-  private signalsService = inject(SignalsService); 
+  private signalsService = inject(SignalsService);
   private hrService = inject(HRService);
   formBuilder = inject(FormBuilder);
 
@@ -22,17 +22,17 @@ export class ClockComponent {
   isNew: boolean = false;
 
   constructor() {
-      effect(() => {
-        this.idBranch = this.signalsService.getBranchSelectedBySidebar()();
-        this.getData();
-      });
-    }
+    effect(() => {
+      this.idBranch = this.signalsService.getBranchSelectedBySidebar()();
+      this.getData();
+    });
+  }
 
   myForm: FormGroup = this.formBuilder.group({
-    clockTolerance: ['', [Validators.required, Validators.minLength(1)], []],
-    delay1: ['', [Validators.required, Validators.minLength(1)], []],
-    delay2: ['', [Validators.required, Validators.minLength(1)], []],
-  })
+    clockTolerance: ['', [Validators.required, Validators.min(120)]],
+    delay1: ['', [Validators.required, Validators.min(0), Validators.max(60)]],
+    delay2: ['', [Validators.required, Validators.min(60), Validators.max(120)]],
+  });
 
   getData() {
     this.hrService.getHRManagementData(this.idBranch).subscribe({
@@ -61,58 +61,73 @@ export class ClockComponent {
   }
 
 
-    onSubmit() {
-      if (this.myForm.invalid) {
-        alerts.basicAlert("Error", "Faltan datos por llenar", "error");
-        return;
-      }
-    
-      const values = this.myForm.value;
-    
-      this.hrData = {
-        ...this.hrData, // conserva id u otros campos
-        ...values       // actualiza con valores nuevos
-      };
-    
-      if (!this.isNew) {
-        // Actualizar datos existentes
-        this.hrService.updateHRManagementData(this.idBranch, this.hrData).subscribe({
-          next: () => {
-            alerts.basicAlert("Actualización", "Los datos fueron guardados exitosamente", "success");
-            this.getData(); // Refrescar datos
-          },
-          error: () => {
-            alerts.basicAlert("Error", "Error al actualizar los datos.", "error");
-          }
-        });
-        return;
-      }
-    console.log(values)
-      // Insertar nuevos datos
-      const payload = {
-        active: true,
-        startDay: "",
-        clockTolerance: values.clockTolerance,
-        delay1: values.delay1,
-        delay2: values.delay2,
-        idBranch: this.idBranch,
-      };
-      console.log(payload)
-    
-      this.hrService.addHRManagementData(payload).subscribe({
+  onSubmit() {
+
+
+    const values = this.myForm.value;
+
+    // Validaciones personalizadas
+    if (values.delay1 < 0 || values.delay1 > 60) {
+      alerts.basicAlert("Error", "El valor de 'delay1' debe estar entre 0 y 60 minutos.", "error");
+      return;
+    }
+    if (values.delay2 < 60 || values.delay2 > 120) {
+      alerts.basicAlert("Error", "El valor de 'delay2' debe estar entre 60 y 120 minutos.", "error");
+      return;
+    }
+    if (values.clockTolerance < 120) {
+      alerts.basicAlert("Error", "El valor de 'clockTolerance' debe ser igual o mayor a 120 minutos.", "error");
+      return;
+    }
+    if (this.myForm.invalid) {
+      alerts.basicAlert("Error", "Faltan datos por llenar", "error");
+      return;
+    }
+
+    this.hrData = {
+      ...this.hrData, // conserva id u otros campos
+      ...values       // actualiza con valores nuevos
+    };
+
+    if (!this.isNew) {
+      // Actualizar datos existentes
+      this.hrService.updateHRManagementData(this.idBranch, this.hrData).subscribe({
         next: () => {
-          alerts.basicAlert("Registro", "Los datos fueron guardados exitosamente", "success");
+          alerts.basicAlert("Actualización", "Los datos fueron guardados exitosamente", "success");
           this.getData(); // Refrescar datos
         },
         error: () => {
-          alerts.basicAlert("Error", "Error al añadir los datos.", "error");
+          alerts.basicAlert("Error", "Error al actualizar los datos.", "error");
         }
       });
-    
-      this.isNew = false;
+      return;
     }
+    console.log(values)
+    // Insertar nuevos datos
+    const payload = {
+      active: true,
+      startDay: "",
+      clockTolerance: values.clockTolerance,
+      delay1: values.delay1,
+      delay2: values.delay2,
+      idBranch: this.idBranch,
+    };
+    console.log(payload)
 
-    revertChanges() {
-      //this.getData(); // Refrescar datos
-    }
- }
+    this.hrService.addHRManagementData(payload).subscribe({
+      next: () => {
+        alerts.basicAlert("Registro", "Los datos fueron guardados exitosamente", "success");
+        this.getData(); // Refrescar datos
+      },
+      error: () => {
+        alerts.basicAlert("Error", "Error al añadir los datos.", "error");
+      }
+    });
+
+    this.isNew = false;
+  }
+
+  revertChanges() {
+    //this.getData(); // Refrescar datos
+  }
+}
