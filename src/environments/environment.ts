9db8f7b4c894416b384746d280822ad9a2b932a1
David Kantun <dkantun@gmail.com>
2025-07-28 20:51:23 -0600

    Imagenes en pdf

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.23.9 (15 Julio 2025 13:04)'
+  version: '2.23.10 (28 Julio 2025 20:51)'
 
 };
 
