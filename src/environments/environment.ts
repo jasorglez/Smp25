0b8a4b4c8eb6056e1d083f149f8c706a6c8ce4e2
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-08-27 17:51:16 -0600

    Update changes finish deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.24.4 (26 Agosto 2025 18:11)'
+  version: '2.24.4 (27 Agosto 2025 18:11)'
 
 };
 
