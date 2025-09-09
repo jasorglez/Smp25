167efc36e6318c8b33569138a93fd838abd3d437
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-09-08 22:11:36 -0600

    fix Requesiciones y OC Update changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.33.1 (08 Septiembre 2025 10:08)'
+  version: '2.33.5 (08 Septiembre 2025 22:08)'
 
 };
 
