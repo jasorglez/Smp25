c01c77a325303be9077f0178a3778a8e9ba2d1a9
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-10-12 13:30:28 -0600

    Update Invoice Electronic RFC by natural or legal person in accounting

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.34.4 (10 Octubre 2025 21:11)'
+  version: '2.34.4 (12 Octubre 2025 14:11)'
 
 };
 
