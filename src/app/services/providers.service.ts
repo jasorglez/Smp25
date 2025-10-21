c0e49aca41f2d876b6815d47ccaf2d3f1d27642a
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-10-20 22:15:21 -0600

    feat: updated daily report service

--- src/app/services/providers.service.ts
+++ src/app/services/providers.service.ts
@@ -19,6 +19,11 @@ export class ProvidersService {
     return this.http.get(`${environment.urlWarehouse}/ProveedorXTabla?idProveedor=${idProvider}&Type=${type}`, { headers: this.trackingService.getHeaders() });
   }
   
+    getMaterXTable(idMaterial: number, type: string ) {
+    return this.http.get(`${environment.urlWarehouse}/ProveedorXTabla?idProveedor=${idMaterial}&Type=${type}`, { headers: this.trackingService.getHeaders() });
+  }
+  
+
   getProviders(idRoot: number) {
     return this.http.get(`${environment.urlSmp}/Providers?idRoot=${idRoot}`, { headers: this.trackingService.getHeaders() });
   }
