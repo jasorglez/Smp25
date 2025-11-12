04add428a52d72129ad30c8cf9888fc009a1ba73
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-11-12 12:45:19 -0600

    update titulos A Formularios service

--- src/app/domains/Almacenes/components/materiales-maestro/materiales-maestro.component.ts
+++ src/app/domains/Almacenes/components/materiales-maestro/materiales-maestro.component.ts
@@ -402,7 +402,7 @@ export class MaterialesMaestroComponent implements OnInit, OnDestroy {
 
       {
         field: 'costo',
-        headerName: 'Costos',
+        headerName: 'Materiales',
         width: 150,
         valueFormatter: (params: any) => {
           return params.value ? `$${params.value.toFixed(2)}` : '$0.00';
