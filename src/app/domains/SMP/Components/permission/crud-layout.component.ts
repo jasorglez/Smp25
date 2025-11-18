17fb5d12c5c0b421f5d0ffd06bbf46f14a56a904
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2025-11-17 23:05:54 -0600

    update permission en root

---
+++ src/app/domains/SMP/Components/permission/crud-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './crud-layout.component.html',
+  styleUrls: ['./crud-layout.component.css']
+})
+export class CrudLayoutComponent { }
