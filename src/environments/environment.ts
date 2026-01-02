d4fb3f9c167f2f6969b4d86c0e592b9cd88aee0c
David Kantun <dkantun@gmail.com>
2026-01-02 09:15:48 -0600

    Arreglando error en reopen ot

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.88.33 (29 Diciembre 2025 23:43)',
+  version: '2.88.33 (02 Enero 2026 09:15)',
 
 };
 
