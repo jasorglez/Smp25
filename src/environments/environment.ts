96ee401ebdb016a066d971c4168a4915d216b556
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-05 17:57:45 -0600

    Update changes environment for deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '2.98.42 (05 Enero 2026 12:00) SKB',
+  version: '2.98.43 (05 Enero 2026 17:50) SKB',
 
 };
 
