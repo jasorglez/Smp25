7f98a3384b89bc5f5e8dd6c0871f1b7cffc336af
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-13 21:52:15 -0600

    Update changes for report and calculate

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -65,7 +65,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '3.10.09 (13 Enero 2025 20:33) - SKB',
+  version: '3.10.09 (13 Enero 2025 22:13) - SKB',
 
 };
 
