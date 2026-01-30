cfdb4ce6cff241591b47c79d4e4843b867579347
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-29 21:09:35 -0600

    fix conflicts

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Ilogin } from '../interface/ilogin';
 import { HttpClient } from '@angular/common/http';
 import  {map} from 'rxjs/operators'
-import { environment } from '../../environments/environment';
+import { environment } from '@env/environment';
 import { TrackingService } from './tracking.service';
 
 
