362925d21cabc7927b40d70614e0d13d0117b6b1
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-30 06:52:54 -0600

    Update changes fix environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -62,7 +62,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '4.13 (30 Enero 2026 06:41) - Metamorphosis',
+  version: '4.14 (30 Enero 2026 06:44) - Metamorphosis',
 
 
 };
