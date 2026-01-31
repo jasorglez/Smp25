1e53d1a959d004779ac7114b6e863840ee3d288b
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-01-31 12:28:10 -0600

    Update changes fix the changes witout for error David in Commit pprevious

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -62,7 +62,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '4.41 (30 Enero 2026 20:25) - Metamorphosis',
+  version: '4.43 (31 Enero 2026 12:57) - Metamorphosis',
 
 
 };
