afce694e7e0a98c4c60cf30b096a504c6a8e3a0b
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-02-24 15:51:05 -0600

    Cambios for TMDB con Reporte Diario

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -70,6 +70,6 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '5.77 (24 Febrero 2026) - Columnas Gastos y Diferencia agregadas al Reporte Facturación',
+  version: '5.77 (24 Febrero 2026 16:00) - Reporte Diario TMDB',
 
 };
