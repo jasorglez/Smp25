ad344865eb32b7c6a083a45b3a92f019557c9409
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-02-27 16:33:03 -0600

    Changes for Workprogram

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -70,7 +70,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '5.83 (27 Febrero 2026 11:00) - Iris',
+  version: '5.92 (27 Febrero 2026 16:35) - Iris',
 };
 
 
