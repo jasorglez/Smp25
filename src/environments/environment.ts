ff1a73230e662a0beb7ccfda8deddf9d287074fd
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-03-24 00:18:07 -0600

    fix: Agregue un reporte de personal para DOn Ricardo EMMPCO

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -73,7 +73,7 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '4.95 (23 Marzo 2026 17:30) - bitácora personal: cols Empleado+Asistencia, materials desc ancha',
+  version: '4.96 (24 Marzo 2026 01:04) - PDF bitácora personal con logos y nombre empresa',
 };
 
 
