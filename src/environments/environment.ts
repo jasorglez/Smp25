283fa02c6a20f7e9402da73c32a8e4710f8876b3
David Kantun <dkantun@gmail.com>
2026-04-06 18:28:09 -0600

    Versión 6.0.18

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -76,5 +76,5 @@ export const environment = {
   urlRefreshToken:
     'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '6.0.17 (03 Abril 2026 16:10) - Diamond',
+  version: '6.0.18 (06 Abril 2026 18:28) - Diamond',
 };
