605e538ba6fbdc9e438c82fd79e7c06913ceb601
David Kantun <dkantun@gmail.com>
2026-04-09 10:05:44 -0600

    Subiendo cambios de endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -27,28 +27,27 @@ export const environment = {
   // urlBpi        : 'https://bi2.centralus.cloudapp.azure.com/bpi/api',
   // urlWarehouse  : 'https://bi2.centralus.cloudapp.azure.com/warehouse/api',
 
-
-  urlSecurity: 'https://bi2.com.mx/api', // 5003
+  urlSecurity: 'https://endpoints.biapp.com.mx/api', // 5003
   //urlSecurity     : 'http://localhost:5260/api',
 
-  urlSmp: 'https://bi2.com.mx/smp/api', // 5004
+  urlSmp: 'https://endpoints.biapp.com.mx/smp/api', // 5004
   //urlSmp          : 'http://localhost:5183/api',
 
-  urlBpi: 'https://bi2.com.mx/bpi/api', // 5005
+  urlBpi: 'https://endpoints.biapp.com.mx/bpi/api', // 5005
 
-  urlWarehouse: 'https://bi2.com.mx/warehouse/api', // 5007
+  urlWarehouse: 'https://endpoints.biapp.com.mx/warehouse/api', // 5007
   //urlWarehouse: 'http://localhost:5199/api',
 
-  urlAdministration: 'https://bi2.com.mx/tracking/api', // 5006 -- Tambien conocido como Tracking
+  urlAdministration: 'https://endpoints.biapp.com.mx/tracking/api', // 5006 -- Tambien conocido como Tracking
   //urlAdministration : 'http://localhost:5047/api', // 5006 -- Tambien conocido como Tracking
 
-  urlNotifications: 'https://bi2.com.mx/notifications/api', // 5011 -- NotificationsTelegram
+  urlNotifications: 'https://endpoints.biapp.com.mx/notifications/api', // 5011 -- NotificationsTelegram
   //urlNotifications: 'http://localhost:5011/api',
 
-  urlChatBot: 'https://bi2.com.mx/telegram/api',
+  urlChatBot: 'https://endpoints.biapp.com.mx/telegram/api',
   //urlChatBot: 'http://localhost:5232/api',
 
-  urlMantenimiento: 'https://bi2.com.mx/maintenance/api',
+  urlMantenimiento: 'https://endpoints.biapp.com.mx/maintenance/api',
 
   root: 'root@bi2.mx',
 
@@ -59,28 +58,32 @@ export const environment = {
     private_key: '	9bb18cadc7bbafd5bd7fb4c2cea3ffb503b0900a',
   },
 
-  urlLogin: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
+  urlLogin:
+    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  urlGetUser: 'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
+  urlGetUser:
+    'https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
   urlFiles: 'gs://beapp-501d1.appspot.com',
 
   urlProfile: './assets/img/profile.png',
 
-  adminFiles: 'http://localhost/sistemas-angular/marketplace/src/assets/img/index.php?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
+  adminFiles:
+    'http://localhost/sistemas-angular/marketplace/src/assets/img/index.php?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  deleteFiles: 'http://localhost/sistemas-angular/marketplace/src/assets/img/delete.php?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
+  deleteFiles:
+    'http://localhost/sistemas-angular/marketplace/src/assets/img/delete.php?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
+  urlRefreshToken:
+    'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
   mercadoPago: {
     // Produccion (ACTIVO)
-     publicKey: 'APP_USR-ffba3d82-7423-4dbc-9012-4ca0571dae19', //Produccion
+    publicKey: 'APP_USR-ffba3d82-7423-4dbc-9012-4ca0571dae19', //Produccion
     // Pruebas (descomenta esta linea y comenta la de Produccion para cambiar manualmente)
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '5.07 (07 Abril 2026 14:09) - sistema de licencias: trial 15 dias, banner aviso, bloqueo y watermark PDF',
+  version:
+    '5.08 (09 Abril 2026 10:05) - sistema de licencias: trial 15 dias, banner aviso, bloqueo y watermark PDF',
 };
-
-
