16235efee690191a2948be296ba8cfaf79551f0c
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-17 18:54:12 -0600

    fix: update branchDelison users.component.ts and environment.ts es para que al menos haya un Root

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -73,6 +73,6 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '3.81 (17 Abril 2026 16:35) - refactor: sidebar sin flujo de contratos ni proyectos',
+  version: '3.82 (17 Abril 2026) - fix: validacion minimo un usuario Root en columna isRoot',
 };
 
