9afd4b99cd357deaea9839ae6b912035e966a120
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-18 13:56:48 -0600

    fix: increase heights - grid 780px, chart 580px

--- src/app/domains/ModProjects/components/advances/advances.component.ts
+++ src/app/domains/ModProjects/components/advances/advances.component.ts
@@ -166,7 +166,7 @@ export class AdvancesComponent implements OnInit, OnChanges {
         }
       ],
       chart: {
-        height: 480,
+        height: 580,
         width: '100%',
         type: "line",
         stacked: false,
@@ -436,7 +436,7 @@ export class AdvancesComponent implements OnInit, OnChanges {
         { name: 'Hitos de Avance',              data: hitosSeries,   type: 'column' }
       ],
       chart: {
-        height: 480,
+        height: 580,
         width: '100%',
         type: "line",
         stacked: false,
