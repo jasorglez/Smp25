4b2b53d9fb26c6e97651cccc33b105d7d223c9b4
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-18 13:15:30 -0600

    chore: update version 5.12

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,5 +85,5 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '5.11 (18 Abril 2026 17:25) font size grid small-text-ag-grid',
+  version: '5.12 (18 Abril 2026 17:35) advance service with token auth + 90 test records',
 };
