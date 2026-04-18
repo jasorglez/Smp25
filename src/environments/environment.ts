40b2eabb18166fc85618000262fdb7b290311170
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-18 13:21:19 -0600

    chore: update version 5.14

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,5 +85,5 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '5.13 (18 Abril 2026 17:50) advances chart improved - area type, better colors, physical=60 delayed',
+  version: '5.14 (18 Abril 2026 18:05) advances chart premium - line, animations, month labels, beautiful design',
 };
