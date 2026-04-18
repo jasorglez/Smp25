033a4e201f09a436a86cd415a1a06678b290488a
David Kantun <dkantun@gmail.com>
2026-04-17 21:52:54 -0600

    Cambios solicitados

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -73,6 +73,6 @@ export const environment = {
 
   urlRefreshToken: 'https://securetoken.googleapis.com/v1/token?key=AIzaSyDxCBGKk8nT09hdW85-PyOkhw5_JPZLF1A',
 
-  version: '3.81 (17 Abril 2026 16:40) - refactor: sidebar sin flujo de contratos ni proyectos',
+  version: '3.82 (17 Abril 2026 21:09) - refactor: sidebar sin flujo de contratos ni proyectos y filtro de sucursal',
 };
 
