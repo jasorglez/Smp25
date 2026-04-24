bf125660c4148600f45ce7caadae386f6b7a9659
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-04-24 11:44:08 -0600

    fix: update expenditure and environment files y la logistica para el contador

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,5 +85,5 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '5.29 (23 Abril 2026) logistica remisiones front inicial con estado REMISION y envio desde pedidos',
+  version: '5.30 (24 Abril 2026) logistica remisiones front inicial con estado REMISION y envio desde pedidos',
 };
