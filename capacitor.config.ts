5d7566745ebc747906f1036d0ce45a101b154427
David Kantun <dkantun@gmail.com>
2026-05-06 11:25:50 -0600

    Android app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.bi.red',
-  appName: 'bi-aug-24',
+  appName: 'Red Ciudadana',
   webDir: 'dist/bi-aug-24/browser'
 };
 
