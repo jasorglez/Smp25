2ece91cac72cd5f9ac36b9f2d05066bedae67ef4
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-05-29 19:45:29 -0600

    fix(pmo): redirect default a programa + tab Actividades reutiliza permiso programa evitando error no autorizado

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,5 +85,5 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '6.95 (29 Mayo 2026) feat(pmo-recursos): descripcion con dropdown dinamico por catalogo',
+  version: '6.96 (29 Mayo 2026) fix(pmo): redirect a programa + tab Actividades usa permiso programa',
 };
