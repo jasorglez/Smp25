42be23a506d89abac9896295d70fabd876d74fed
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-03 00:20:19 -0600

    chore: actualiza texto version v7.86

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,5 +85,5 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '7.86 (03 Junio 2026) fix(crm): quita Pipeline nav duplicado Kanban ya esta en Dashboard',
+  version: '7.86 (03 Junio 2026) fix(crm): Kanban, CRM en Dashboard',
 };
