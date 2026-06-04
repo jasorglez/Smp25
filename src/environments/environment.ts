6f9efdb5099c9d599b5a0e25b297a21086897634
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-04 07:07:32 -0600

    fix(municipio): dashboards-pal Ingresos Egresos Total compacto N2 v8.11

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,7 +85,7 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '8.10 (04 Junio 2026) fix(municipio): submenu palacio-municipal texto compacto N2',
+  version: '4.10 (04 Junio 2026) fix(municipio): submenu palacio-municipal texto compacto N2',
 };
 
 
