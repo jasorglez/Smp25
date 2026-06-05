819ca2e8c61f6b92e870657bf469883b09fd9b40
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-05 14:40:30 -0600

    chore(version): bump environment version to 4.30

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,7 +85,7 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '4.29 (05 Junio 2026) feat(prospectos): municipios por estado en dropdown dependiente',
+  version: '4.30 (05 Junio 2026) fix(logistica): acciones visibles en detalle de pedidos',
 };
 
 
