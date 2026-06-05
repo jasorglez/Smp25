58269d9bbf79281cc196ac6e11c107509bb814f2
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-05 11:18:43 -0600

    fix(prospectos): grid corregido -10% -5% v4.21

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,7 +85,7 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '4.20 (05 Junio 2026) fix(prospectos): grid -10%-7% alto',
+  version: '4.21 (05 Junio 2026) fix(prospectos): grid -10%-5% alto',
 };
 
 
