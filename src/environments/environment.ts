92e5921859cbf53ea8dc595f0a9b833498f88f66
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-08 18:37:40 -0600

    debug(expenditure): console.log flujo comprobante v4.61

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,7 +85,7 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '4.60 (08 Junio 2026) fix(expenditure): monto comprobante robusto strips $ y comas',
+  version: '4.61 (08 Junio 2026) debug(expenditure): logs comprobante para diagnostico',
 };
 
 
