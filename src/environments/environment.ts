62a3ed3e5a31f13991e02cd0ff14c30df112d214
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-08 19:47:02 -0600

    fix(expenditure): didOpen captura archivo Swal v4.70

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -85,7 +85,7 @@ export const environment = {
     // publicKey: 'TEST-4399133a-4676-42a2-ab85-d946370bf96a',
   },
 
-  version: '4.69 (08 Junio 2026) debug(expenditure): logs Swal result y HTTP',
+  version: '4.70 (08 Junio 2026) fix(expenditure): didOpen captura archivo Swal',
 };
 
 
