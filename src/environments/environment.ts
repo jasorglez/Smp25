fbebf2c589a37036d234641a6b946c269eabad89
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-19 14:43:35 -0600

    feat(chatbot): OTs abiertas/cerradas y por tipo en agente v5.76

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -88,5 +88,5 @@ export const environment = {
 
   geminiApiKey: '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',
 
-  version: '5.75 (19 Junio 2026) feat(chatbot): reporte diario de campo en agente',
+  version: '5.76 (19 Junio 2026) feat(chatbot): OTs en agente',
 }; 
