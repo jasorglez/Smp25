49c8be1ca0c38d1c2a0d2f29ffe2595a1fd48e06
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-06-27 07:33:42 -0600

    Corregir seguridad de Control Diario IA

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -89,5 +89,5 @@ export const environment = {
   geminiApiKey: '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',
   gnewsApiKey: '94ccc596b4dfebc3f72683b6e81d81bc',
 
-  version: '4.33 (26 Junio 2026) Control Diario IA Telegram',
+  version: '4.34 (27 Junio 2026) Seguridad Control Diario IA',
 }; 
