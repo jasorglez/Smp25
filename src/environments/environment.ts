418ea601b033c6d48f7f9f718e11537736f50301
Jose Angel Soriano Gonzalez <jsorglez@gmail.com>
2026-09-10 17:47:07 -0400

    style: compact subcontractor selector

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -83,5 +83,5 @@ export const environment = {
   geminiApiKey: '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',
   gnewsApiKey: '94ccc596b4dfebc3f72683b6e81d81bc',
 
-  version: '4.99.92 (10 Septiembre 2026) inventario por almacén y movimientos',
+  version: '4.99.93 (10 Septiembre 2026) combo de subcontratistas compacto',
 };
